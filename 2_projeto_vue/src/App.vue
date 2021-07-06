f392b255d92a7144f40be3c9a775a68d8919427e
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-06 14:29:09 -0300

    Lifecycle hooks

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <PrimeiroComponente />
+  <Lifercycle />
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'; 
+import Lifercycle from './components/Lifecycle.vue'; 
   export default {
     name: "App",
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      Lifercycle
     }
   }
 </script>
