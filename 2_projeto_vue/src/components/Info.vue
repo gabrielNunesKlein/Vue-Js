2df798d7c82fe475db06be8d81a1e4928f7417c2
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-07 13:07:29 -0300

    Atributos dinâmicos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -9,17 +9,24 @@
             <li>JavaScript</li>
         </ul>
         <p v-show="mostrar_email">Mande uma menssagem para: {{ email }}</p>
+        <p>Acesse meu portifolio <a v-bind:href="meu_portifolio" target="_blank">click aqui</a></p>
+        <Perfil />
     </div>
 </template>
 
 <script>
+import Perfil from './Perfil.vue';
 export default{
     name: "Info",
+    components: {
+        Perfil
+    },
     data(){
         return {
             esta_trabalhando: false,
             mostrar_email: true,
-            email: 'Gabriel.klein@gmail.com'
+            email: 'Gabriel.klein@gmail.com',
+            meu_portifolio: 'https://github.com/gabrielNunesKlein'
         }
     }
 }
---
+++ 2_projeto_vue/src/components/Perfil.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="avatar" :alt="descricao">
+</template>
+
+<script>
+    export default {
+        name: "Perfil",
+        data(){
+            return {
+                avatar: '/img/avatar-blank.png',
+                descricao: 'Gabriel Klein'
+            }
+        }
+    }
+</script>
