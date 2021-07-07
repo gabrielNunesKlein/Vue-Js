5b1573cf34b8c5d5aa2d2833ac2ef492c2643ff5
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-07 13:57:46 -0300

    Métodos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -8,6 +8,9 @@
             <li>Java</li>
             <li>JavaScript</li>
         </ul>
+        <div>
+            <button @click="mostrarEmail">{{ textoBotao }}</button>
+        </div>
         <p v-show="mostrar_email">Mande uma menssagem para: {{ email }}</p>
         <p>Acesse meu portifolio <a v-bind:href="meu_portifolio" target="_blank">click aqui</a></p>
         <Perfil />
@@ -24,9 +27,20 @@ export default{
     data(){
         return {
             esta_trabalhando: false,
-            mostrar_email: true,
+            mostrar_email: false,
             email: 'Gabriel.klein@gmail.com',
-            meu_portifolio: 'https://github.com/gabrielNunesKlein'
+            meu_portifolio: 'https://github.com/gabrielNunesKlein',
+            textoBotao: 'Mostrar E-mail'
+        }
+    },
+    methods: {
+        mostrarEmail(){
+            this.mostrar_email = !this.mostrar_email;
+            if(!this.mostrar_Email){
+                this.textoBotao = 'Mostrar E-mail';
+            }else{
+                this.textoBotao = 'Esconder E-mail';
+            }
         }
     }
 }
