21bd3ba37617cafe979d9c7b33206a2fcf32c8b7
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-07 17:15:58 -0300

    CSS Global e CSS Scoped

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <Header />
   <PrimeiroComponente />
   <Lifercycle />
   <Pessoa />
@@ -7,15 +8,27 @@
 </template>
 
 <script>
-import PrimeiroComponente from './components/PrimeiroComponente.vue'; 
+import PrimeiroComponente from './components/PrimeiroComponente.vue';
+import Header from './components/Header.vue'; 
 import Lifercycle from './components/Lifecycle.vue'; 
 import Pessoa from './components/Pessoa.vue';
   export default {
     name: "App",
     components: {
+      Header,
       PrimeiroComponente,
       Lifercycle,
       Pessoa
     }
   }
 </script>
+
+<style>
+  body{
+    background-color: #333;
+    color: #fff;
+  }
+  a{
+    color: red;
+  }
+</style>
