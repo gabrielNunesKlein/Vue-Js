48442d026285ae634ae16a915f7d3fb499d335ac
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-08 14:54:39 -0300

    Eventos @submit

--- 2_projeto_vue/src/components/Form/Input.vue
+++ 2_projeto_vue/src/components/Form/Input.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
-        <div>
-            <input type="text">
-        </div>
-        <div>
-            <input type="text">
-        </div>
-        <div>
-            <Submit />
-        </div>
+        <h1>Meu Formulario</h1>
+        <form @submit="enviarFormulario($event)">
+            <div>
+                <input type="text" v-model="nome">
+            </div>
+            <div>
+                <input type="text" v-model="email">
+            </div>
+            <div>
+                <Submit />
+            </div>
+        </form>
     </div>
 </template>
 
@@ -16,8 +19,24 @@
 import Submit from './Submit.vue';
     export default {
         name: 'Input',
+        data(){
+            return {
+                nome: "",
+                email: ""
+            }
+        },
         components: {
             Submit
+        },
+        methods: {
+            enviarFormulario(e){
+                e.preventDefault();
+
+                console.log("Formulario enviado")
+                const nome = this.nome;
+                const email = this.email;
+                console.log(`Nome: ${nome}, Email: ${email}`);
+            }
         }
     }
 </script>
