7198595e3f97e8ac4609fd7e4896102ad56e4179
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-08 15:16:10 -0300

    Multiplos eventos

---
+++ 2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1), segundo(txt2), terceiro()">Ativar Eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'MultiplosEventos',
+        data(){
+            return{
+                multiplos_eventos: "",
+                txt1: "Primeiro evento !",
+                txt2: "Segundo Evento !"
+            }
+        },
+        methods:{
+            primeiro(txt){
+                this.multiplos_eventos = txt;
+            },
+            segundo(txt){
+                setTimeout(() => this.multiplos_eventos = txt, 500)
+            },
+            terceiro(){
+                setTimeout(() => this.multiplos_eventos = "", 1500)   
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    div{
+        padding: 100px;
+    }
+</style>
